65b2d94e5cb3f92dd233150489ed8493779a0062
Balaussa <miss_balausa05@inbox.ru>
2023-07-01 03:49:39 +0600

    release v1

---
+++ js/script.js
@@ -0,0 +1,26 @@
+// "use strict";
+
+
+// let number = 5;
+// const leftBorderWidth = 1;
+
+// number = 10;
+// console.log(number);
+
+// const arr = {
+//     a: 1,
+//     1: 'b',
+//     2: 'c'
+// };
+
+// const b = 'k';
+
+// arr[b] = '1234';
+
+// console.log(arr.k);
+
+const isChecked = true,
+      isClosed = true,
+      isMine = false;
+
+console.log(isChecked && isClosed && isMine);
